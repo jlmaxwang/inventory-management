5ee9b378305b51143fd5ce3ffbad3ad31e4b742d
Max Jilu Wang <wangjilu23@gmail.com>
2022-03-24 21:01:07 +1100

    added get,post for import/export

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :powders do
     collection do
-      get :import
+      get :import, :export
+      post :import_powder, :export_powder
     end
   end
 end
